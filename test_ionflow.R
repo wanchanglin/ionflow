815c6df56411dfeefdb18ccd43a219b57c6e9840
wanchanglin <wanchang_lin@outlook.com>
2020-10-11 16:42:17 +0100

    test PreProcessing

--- test_ionflow.R
+++ test_ionflow.R
@@ -1,49 +1,34 @@
-#' wl-03-07-2020, Fri: Load packages here.
-#' wl-03-07-2020, Fri: qgraph loads plenty of R packages
-#' wl-06-07-2020, Mon: debug functions PreProcessing and ExploratoryAnalysis
-#' wl-30-07-2020, Thu: test on subset of IonData
-#' wl-02-09-2020, Wed: test preProcessing with new data set
+#' wl-11-10-2020, Sun: test PreProcessing
 
 ## ==== General settings ====
 rm(list = ls(all = T))
-#' set.seed(123)
 
-#' tool_dir <- "C:/R_lwc/r_data/icl/"
-tool_dir <- "~/R_lwc/r_data/icl/"
+tool_dir <- "~/my_galaxy/ionflow/"       #' wl: change here
+
 setwd(tool_dir)
 pkgs <- c("optparse", "reshape2", "plyr", "dplyr", "tidyr", "ggplot2", 
           "ggrepel", "corrplot", "gplots", "network", "sna", "GGally",
           "org.Sc.sgd.db", "GO.db", "GOstats")
 invisible(lapply(pkgs, library, character.only = TRUE))
-source("funcs_ionflow.R")
+source("funcs_ionflow_ji.R")
 
 ## ==== Data preparation ====
 
-#' data for annotations
-lib_dir <- paste0(tool_dir, "libraries/")
-data_GOslim <- read.table("./libraries/data_GOslim.tsv", sep = "\t", header = T)
-data_ORF2KEGG <- read.table("./libraries/data_ORF2KEGG.tsv", sep = "\t", header = T)
-
-#' Load data set
-#' ion_data <- read.table("./test-data/ionome_oe_test.tsv", header = T, sep = "\t")
-ion_data <- read.table("./test-data/ionome_ko_test.tsv", header = T, sep = "\t")
+ion_data <- read.table("./test-data/iondata.tsv", header = T, sep = "\t")
+#' ion_data <- read.table("./test-data/ionome_oe.tsv", header = T, sep = "\t")
 #' ion_data <- read.table("./test-data/ionome_ko.tsv", header = T, sep = "\t")
-#' ion_data <- read.table("./test-data/iondata_test.tsv", header = T, sep = "\t")
-#' ion_data <- read.table("./test-data/iondata.tsv", header = T, sep = "\t")
-
-#' std_data <- read.table("./test-data/user_std.tsv", header = T, sep = "\t")
-std_data <- NULL
 
 ## ==== Pre-processing ====
-## names(ion_data)
-##  [1] "orf_id_oe"   "plate_id_oe" "trays_id_oe" "batch_id_oe" "As"
-##  [6] "Ca"          "Cd"          "Cl"          "Co"          "Cu"
-## [11] "Fe"          "K"           "Mg"          "Mn"          "Mo"
-## [16] "Na"          "Ni"          "P"           "S"           "Se"
-## [21] "Zn"
-pre_proc <- PreProcessing(data = ion_data, stdev = std_data,
-                          var_id = 1, batch_id = 4, data_id = 5)
-## wl-02-09-2020, Wed: can select batch_id as 3 or 4
+pre_proc <- PreProcessing(data = ion_data, 
+                          var_id = 1, batch_id = 2, data_id = 3,
+                          method_norm = "median",
+                          control_lines = NULL,
+                          control_use = "control",
+                          method_outliers = "mad",
+                          n_thrs = 4,
+                          stand_method = "std",
+                          stdev = NULL, symb_thr = 4, p_symb = 0.5)
+
 
 pre_proc$stats.raw_data
 pre_proc$stats.outliers
@@ -67,6 +52,11 @@ exp_anal$plot.correlation_network
 head(exp_anal$data.PCA_loadings)
 
 ## ==== Gene Clustering ====
+#' data for annotations
+lib_dir <- paste0(tool_dir, "libraries/")
+data_GOslim <- read.table("./libraries/data_GOslim.tsv", sep = "\t", header = T)
+data_ORF2KEGG <- read.table("./libraries/data_ORF2KEGG.tsv", sep = "\t", header = T)
+
 gene_clust <- GeneClustering(data = pre_proc$data.wide,
                              data_symb = pre_proc$data.wide_symb,
                              thres_clus = 10, thres_anno = 5)
@@ -84,4 +74,3 @@ gene_net$plot.pnet
 gene_net$plot.impact_betweenness
 gene_net$stats.impact_betweenness
 gene_net$stats.impact_betweenness_tab
-

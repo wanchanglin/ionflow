6cee9c913ce1d88dda0f024e07e0978be23752cd
wanchanglin <wanchang_lin@outlook.com>
2020-11-07 07:28:09 +0000

    test enrichment

--- test_ionflow.R
+++ test_ionflow.R
@@ -3,12 +3,13 @@
 #' wl-21-10-2020, Wed: test enrichment analysis
 #' wl-26-10-2020, Mon: test GeneNetwork with cosM
 #' wl-30-10-2020, Fri: test some changes
+#' wl-07-11-2020, Sat: test enrichment
 
 ## ==== General settings ====
 rm(list = ls(all = T)) 
 
-tool_dir <- "~/my_galaxy/ionflow/"
-#' tool_dir <- "C:/R_lwc/my_galaxy/ionflow/"
+#' tool_dir <- "~/my_galaxy/ionflow/"
+tool_dir <- "C:/R_lwc/my_galaxy/ionflow/"
 
 setwd(tool_dir)
 pkgs <- c("optparse", "reshape2", "plyr", "dplyr", "tidyr", "ggplot2",
@@ -21,10 +22,8 @@ source("funcs_ionflow.R")
 
 #' ion_data <- read.table("./test-data/iondata_test.tsv", header = T, sep = "\t")
 #' ion_data <- read.table("./test-data/iondata.tsv", header = T, sep = "\t")
-#' ion_data <- read.table("C:/R_lwc/r_data/icl/test-data/ionome_ko.tsv", header = T, sep = "\t")
-ion_data <- read.table("~/R_lwc/r_data/icl/test-data/ionome_ko_test.tsv", header = T, sep = "\t")
-#' ion_data <- read.table("~/R_lwc/r_data/icl/test-data/ionome_oe.tsv", header = T, sep = "\t")
-#' ion_data <- read.table("~/R_lwc/r_data/icl/test-data/ionome_oe_test.tsv", header = T, sep = "\t")
+#' ion_data <- read.table("C:/R_lwc/r_data/icl/test-data/ionome_ko_test.tsv", header = T, sep = "\t")
+ion_data <- read.table("C:/R_lwc/r_data/icl/test-data/ionome_oe_test.tsv", header = T, sep = "\t")
 
 ## ==== Pre-processing ====
 pre_proc <- PreProcessing(data = ion_data,
@@ -33,7 +32,7 @@ pre_proc <- PreProcessing(data = ion_data,
                           method_outliers = "IQR",
                           n_thrs = 3,
                           stand_method = "std",
-                          stdev = NULL, symb_thr = 3)
+                          stdev = NULL, symb_thr = 2)
 
 head(pre_proc$data.gene.zscores)
 head(pre_proc$data.gene.symb)
@@ -66,12 +65,11 @@ exp_anal <- ExploratoryAnalysis(data = data)
 #' head(exp_anal$data.PCA_loadings)
 
 ## ==== Gene Network ====
-
 gene_net <- GeneNetwork(data = data,
                         data_symb = data_symb,
-                        min_clust_size = 5, thres_corr = 0.6,
-                        #' method_corr = "cosine")
+                        min_clust_size = 10, thres_corr = 0.60,
                         method_corr = "pearson")
+                        #' method_corr = "cosine")
                         #' method_corr = "hybrid_mahal_cosine")
                         #' method_corr = "mahal_cosine")
 gene_net$plot.pnet
@@ -104,10 +102,8 @@ gene_clust$stats.Goterms_enrichment
 
 ## =========================================================================
 #' wl-21-10-2020, Wed: DEBUG stuff
-
+#' for PreProcessing
 if (F) {
-
-  #' for PreProcessing
   data = ion_data
   var_id = 1
   batch_id = 2
@@ -121,23 +117,4 @@ if (F) {
   stand_method = "std"
   stdev = NULL
   symb_thr = 4
-
-  #' For GeneNetwork
-  data = pre_proc$data.gene.zscores
-  data_symb = pre_proc$data.gene.symb
-  min_clust_size = 10
-  thres_corr = 0.6
-  method_corr = c("pearson", "spearman", "kendall", "cosine",
-                  "mahal_cosine", "hybrid_mahal_cosine")
-
-  #' for enrichment analysis
-  data            <- pre_proc$data.gene.symb
-  min_clust_size  <- 5
-  annot_pkg       <- "org.Sc.sgd.db"
-  pval            <- 0.05
-  ont             <- "BP"
-  f_max           <- F
-  x               <- data[, -1]
-
-  gene_clus(x, min_clust_size = 5, f_max = F)
 }
--- test_ionflow_human.R
+++ test_ionflow_human.R
@@ -4,7 +4,8 @@
 ## ==== General settings ====
 rm(list = ls(all = T)) 
 
-tool_dir <- "~/my_galaxy/ionflow/"
+#' tool_dir <- "~/my_galaxy/ionflow/"
+tool_dir <- "C:/R_lwc/my_galaxy/ionflow/"
 
 setwd(tool_dir)
 pkgs <- c("optparse", "reshape2", "plyr", "dplyr", "tidyr", "ggplot2",
@@ -14,30 +15,36 @@ invisible(lapply(pkgs, library, character.only = TRUE))
 source("funcs_ionflow.R")
 
 ## ==== Data preparation ====
-
 dat <- read.table("./test-data/human_z_profiles.csv", header = T, sep = ",")
 dat <- dat[!duplicated(dat$Gene), ]
 colnames(dat)[1] <- "Line"
-dim(dat)
+dat_symb <- symbol_data(x = dat, symb_thr = 2)
 
-data_symb <- symbol_data(x = dat, symb_thr = 2)
+#' filtering: Select phenotypes of interest
+if (F) {
+  idx      <- rowSums(abs(dat_symb[, -1])) > 0
+  dat      <- dat[idx, ]
+  dat_symb <- dat_symb[idx, ]
+}
+
+dim(dat)
+dim(dat_symb)
 
 ## ==== Exploratory analysis ====
 exp_anal <- ExploratoryAnalysis(data = dat)
 
 ## ==== Gene Network ====
-
 gene_net <- GeneNetwork(data = dat,
-                        data_symb = data_symb,
+                        data_symb = dat_symb,
                         min_clust_size = 5, thres_corr = 0.6,
-                        #' method_corr = "cosine")
                         method_corr = "pearson")
+                        #' method_corr = "cosine")
                         #' method_corr = "hybrid_mahal_cosine")
                         #' method_corr = "mahal_cosine")
 gene_net$plot.pnet
 
 #' ==== GO/KEGG enrichment analysis ====
-kegg_en <- kegg_enrich(data = data_symb, min_clust_size = 5,
+kegg_en <- kegg_enrich(data = dat_symb, min_clust_size = 5,
                        pval = 0.05, annot_pkg = "org.Hs.eg.db")
 kegg_en
 

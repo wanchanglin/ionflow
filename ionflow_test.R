26b1c66474657a90496033567c69eca34ec78314
wanchanglin <wanchang_lin@outlook.com>
2021-06-07 11:45:23 +0100

    update Jacopo's new changes

--- ionflow_test.R
+++ ionflow_test.R
@@ -1,97 +1,151 @@
-#' wl-11-10-2020, Sun: test PreProcessing
-#' wl-26-10-2020, Mon: test cosM
-#' wl-07-11-2020, Sat: test enrichment
-#' wl-09-11-2020, Mon: test GeneNetwork
-#' wl-16-12-2020, Wed: test enrichment using network community centre
-#' wl-23-03-2021, Tue: tidy up for version 3
+#' wl-07-06-2021, Mon: Use jacopo's 'tutorial_galaxy_ionflow.R'
 
 ## ==== General settings ====
+
 rm(list = ls(all = T))
 tool_dir <- "~/my_galaxy/ionflow/"
 setwd(tool_dir)
 pkgs <- c("optparse", "reshape2", "plyr", "dplyr", "tidyr", "ggplot2",
           "ggrepel", "corrplot", "gplots", "network", "sna", "GGally",
-          "org.Sc.sgd.db", "GO.db", "GOstats", "KEGG.db", "pheatmap")
+          "org.Sc.sgd.db","org.Hs.eg.db","GO.db", "GOstats", "KEGG.db",
+          "pheatmap")
 invisible(lapply(pkgs, library, character.only = TRUE))
 source("ionflow_funcs.R")
 
-## ==== Data preparation ====
+## ==== S.Cerevisia Ionome ====
 
-#' ion_data <- read.table("./test-data/iondata_test.tsv", header = T, sep = "\t")
-#' ion_data <- read.table("./test-data/iondata.tsv", header = T, sep = "\t")
-ion_data <- read.table("~/R_lwc/r_data/icl/test-data/ionome_oe_test.tsv",
-                       header = T, sep = "\t")
+ion_data <- read.table("./test-data/Dataset_IonFlow_Ionome_KO_short.csv", 
+                       header = T, sep = ",")
 
-#' Test for batch control
-#' idx <- ion_data[, 1] %in% "BY4741"
-#' sum(idx)
-#' control_lines =  "BY4741"    #' only for inome_ko'
-#' control_use = "all",
+## ==== Data preprocessing ====
 
-#' Test for cumtom standardisation
-#' stdev <- read.table("./test-data/user_std.tsv", header = T, sep = "\t")
-#' stand_method = "custom"
-#' stdev = stdev
-
-## ==== Pre-processing ====
 pre <- PreProcessing(data = ion_data,
-                     var_id = 1, batch_id = 4, data_id = 5,
+                     var_id = 1, batch_id = 3, data_id = 5,
                      method_norm = "median",
-                     control_lines = NULL,
-                     method_outliers = "IQR",
+                     control_lines = "BY4741",
+                     control_use = "all",
+                     method_outliers = "log.FC.dist",
                      thres_outl = 3,
                      stand_method = "std",
                      stdev = NULL,
-                     thres_symb = 3)
+                     thres_symb = 2)
+## names(pre)
+##  [1] "stats.raw.data"    "stats.outliers"    "stats.batches"
+##  [4] "stats.std"         "data.long"         "data.line.logFC"
+##  [7] "data.line.zscores" "data.line.symb"    "plot.overview"
+## [10] "plot.hist"         "plot.change.stat"  "plot.change.dir"
+## [13] "plot.medians"      "plot.CV
 
-head(pre$data.gene.zscores)
-head(pre$data.gene.symb)
+pre$stats.raw.data
+pre$stats.outliers
+pre$stats.batches
+pre$stats.std
+head(pre$data.line.zscores)
+head(pre$data.line.symb)
+head(pre$data.long)
 
-#' ==== Filter data set ====
-dat      <- pre$data.gene.zscores
-dat_symb <- pre$data.gene.symb
-dim(dat)
+# z-score distributions 
+pre$plot.hist
+# processed samples overview 
+pre$plot.overview 
+# batch median log-concentrations 
+pre$plot.medians  
+# CV batch median log-concentrations
+pre$plot.CV  
+# Histogram Number of Elements Changed
+pre$plot.change.stat  
+# Histogram Number of Changes per Element
+pre$plot.change.dir  
 
-#' Select phenotypes of interest
-idx      <- rowSums(abs(dat_symb[, -1])) > 0
-dat      <- dat[idx, ]
-dat_symb <- dat_symb[idx, ]
-dim(dat)
+## ==== Exploratory Analysis Plot ====
 
-## ==== Exploratory analysis ====
-expl <- ExploratoryAnalysis(data = dat)
+expl <- IonAnalysis(data = pre$data.line.zscores, thres_ion_corr = 0.15)
+## names(expl)
+## [1] "data.pca.load" "plot.pca" "plot.corr" "plot.net" "plot.heat"
+
+# PCA
 expl$plot.pca
-expl$plot.corr
-expl$plot.corr.heat
-expl$plot.heat
+# Ion-ion correlation network
 expl$plot.net
-head(expl$data.pca.load)
+# Ions heatmap
+expl$plot.heat
 
-## ==== Gene Network ====
-gene_net <- GeneNetwork(data = dat,
-                        data_symb = dat_symb,
-                        min_clust_size = 10,
-                        thres_corr = 0.75,
-                        method_corr = "pearson")
-                        #' method_corr = "mahal_cosine")
-names(gene_net)
-gene_net$plot.pnet1    #' symbolic pheno (hclust)
-X11()
-gene_net$plot.pnet2    #' network (comminuty detection)
+## ==== Clustering ====
 
-gene_net$plot.impact_betweenness
-gene_net$stats.impact_betweenness
-gene_net$stats.impact_betweenness_tab
-head(gene_net$net_node)
+gcl <- ProfileClustering(pre$data.line.symb, min_clust_size = 10,
+                         h_tree = 0, filter_zero_string = TRUE)
+## names(gcl)
+## [1] "clusters.vector" "tab.clusters" "tab.clusters.subset"
+          
+head(gcl$clusters.vector)
+head(gcl$tab.clusters)
+head(gcl$tab.clusters.subset)
 
-## ==== GO/KEGG enrichment ====
-#' 2 - symbolic cluster. 3 - network community centre
-clus_id = 2
-mat <- gene_net$net_node[, c(1, clus_id)] 
+# select larger clusters
+cluster_vector <- 
+  gcl$clusters.vector[gcl$clusters.vector$Cluster %in%
+                      gcl$tab.clusters.subset$Cluster, ]
 
-kegg <- kegg_enrich(mat = mat, pval = 0.05, annot_pkg = "org.Sc.sgd.db")
-kegg
+# extract symbolic and z-score prifiles for lines in selected clusters
+symbol_profiles <- pre$data.line.symb
+symbol_profiles$Cluster <- 
+  cluster_vector$Cluster[match(symbol_profiles$Line, cluster_vector$Line)]
 
-go  <- go_enrich(mat = mat, pval = 0.05, ont = "BP", annot_pkg = "org.Sc.sgd.db")
-go
+zscore_profiles <- pre$data.line.zscores
+zscore_profiles$Cluster <- 
+  cluster_vector$Cluster[match(zscore_profiles$Line, cluster_vector$Line)]
 
+# remove lines showing no phenotype 
+symbol_profiles <- symbol_profiles[!is.na(symbol_profiles$Cluster),]
+zscore_profiles <- zscore_profiles[!is.na(zscore_profiles$Cluster),]
+
+mat_long <- reshape2::melt(zscore_profiles, id = c("Line", "Cluster"),
+                 variable.name = "Ion", value.name = "zscore")
+
+mat_long$n.genes <- 
+  gcl$tab.clusters.subset$Number.of.genes[match(mat_long$Cluster, 
+                                                gcl$tab.clusters.subset$Cluster)]
+mat_long$title <- paste0('Cluster ', mat_long$Cluster,' (', 
+                         mat_long$n.genes, ' genes)')
+
+p_gcl <- 
+  ggplot(data = mat_long, aes(x = Ion, y = zscore, group = Line), color = "gray") +
+  geom_line() +
+  stat_summary(fun.data = "mean_se", color = "red", geom = "line", group=1) +
+  labs(x = "", y = "z-score") +
+  coord_cartesian(ylim = c(-8, 8)) +
+  facet_wrap(~title) +
+  theme(legend.position = "none",
+        axis.text.x = element_text(angle = 90, hjust = 1),
+        axis.text = element_text(size = 10))
+p_gcl
+
+## ==== Enrichment Analysis ====
+
+# Input ORFs for yeast and ENTREZIDs for mouse or human
+
+# BP
+ge <- GOEnricher(cluster_vector, pval = 0.05, min_count = 3, 
+                 annot_pkg = "org.Sc.sgd.db", ont = "BP",
+                 gene_uni = as.character(pre$data.line.zscores$Line)) 
+## names(ge)
+## [1] "enrichment.summary" "enrichment.full.results"
+ge$enrichment.summary
+
+## ==== Network Analysis ====
+
+gn <- GeneticNetwork(data = zscore_profiles, 
+                     method_corr = "cosine",
+                     thres_corr = 0.7,
+                     network_modules = "input",
+                     cluster_vector = cluster_vector,
+                     cluster_label_vector = NULL)
+## names(gn)
+## [1] "network" "network.modules"
+## [3] "plot.network" "plot.impact_betweenness"
+## [5] "stats.impact_betweenness"
+
+# Plot Network 
+gn$plot.network
+# Plot Impact vs Betwennes
+gn$plot.impact_betweenness
